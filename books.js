87dc2ea551a89342fc6946d761763d42ea52a315
Busiwa24 <busiwaliuma@gmail.com>
2021-08-16 18:57:24 +0200

    create javasript object

--- books.js
+++ books.js
@@ -12,3 +12,26 @@ function getBooks() {
   }
   return myBooks;
 }
+
+function showBooks() {
+  bookSection.innerHTML = '';
+  const myBooks = getBooks();
+  for (let i = 0; i < myBooks.length; i += 1) {
+    const book = myBooks[i];
+    const bookList = `
+    <div id="book-info">
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    <button id=${i} type="submit" class="remove-book" onclick="removeBook(this.id)">Remove</button>
+    <hr> 
+    </div>
+    `;
+    bookSection.innerHTML += bookList;
+  }
+}
+
+
+function clearInput() {
+  bookTitle.value = '';
+  bookAuthor.value = '';
+}
