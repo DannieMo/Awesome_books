da6754939e2196a13c0f2e657675d5213b776cdd
Busiwa24 <busiwaliuma@gmail.com>
2021-08-17 17:51:27 +0200

    Add class page

--- books.js
+++ books.js
@@ -11,4 +11,29 @@ class AwesomeBooks {
   }
 }
 
-
+class Page {
+  // Check if books are in LocalStorage
+  static getBooks() {
+    let myBooks;
+    if (localStorage.getItem('newBooks') === null) {
+      myBooks = [];
+    } else {
+      myBooks = JSON.parse(localStorage.getItem('newBooks'));
+    }
+    return myBooks;
+  }
+  // Div that contains books HTML
+  static showBooks() {
+    bookSection.innerHTML = '';
+    const myBooks = this.getBooks();
+    for (let i = 0; i < myBooks.length; i += 1) {
+      const book = myBooks[i];
+      const bookList = `
+    <div id="book-info">
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    <button id=${i} type="submit" class="remove-book" onclick="removeBook(this.id)">Remove</button>
+    <hr> 
+    </div>
+    `;
+      bookSection.innerHTML += bookList;
